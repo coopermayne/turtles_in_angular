119080061894a9a35add3d1f963e5166de2104b7
Cooper Mayne <coopermayne@gmail.com>
2014-03-03 18:38:03 -0500

    wooooo got it working with angular

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,115 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 var app = angular.module('myApp', [])
 
 app.controller('appCtrl', function($scope) {
 });
 
-app.controller('appCtrl', function($scope) {
-  $scope.tSettings = [
-    {name: 'dragon', age: 12},
-    {name: 'noder', age: 22},
-    {name: 'puppy', age: 32}
-  ]
-  $scope.test = "something";
+app.controller('appCtrl', function($scope,Favorites) {
+  $scope.favorites = Favorites;
+  $scope.options = Favorites[0];
+  $scope.change = function() {
+    console.log($scope.options);
+  };
+
+  $scope.render = function(options) {
+    var t = new Turtle(options);
+  }
+
+  $scope.$watch('options',function() {
+    console.log($scope.options);
+    $scope.render($scope.options);
+  }, true)
+});
+
+app.directive('supersuper', function() {
+  return function(scope, element, attr) {
+  };
+});
+
+app.directive('previewangle', function() {
+  return function(scope, element, attr) {
+  };
+});
+
+app.factory('Favorites', function() {
+  return [
+    {
+      id: 0,
+      name: 'Sierpinski Triangle',
+      axiom: 'FX',
+      rules: [
+        {
+          input: 'X',
+          output: 'YlFXlFY'
+        }, {
+          input: 'Y',
+          output: 'XrFYrFX'
+        }, {
+          input: null,
+          output: null
+        }
+      ],
+      iterations: 6,
+      angle: 60
+    },
+    {
+      id: 1,
+      name: 'Dragon',
+      axiom: "FX",
+      rules: [
+        {
+          input: 'X',
+          output: 'XlYFl'
+        }, {
+          input: 'Y',
+          output: 'rFXrY'
+        }, {
+          input: null,
+          output: null
+        }
+      ],
+      iterations: 6,
+      angle: 90
+    },
+    {
+      id: 2,
+      name: 'Plant',
+      axiom: 'FX',
+      rules: [
+        {
+          input: 'X',
+          output: 'Fr[[X]lX]lF[lFX]rX'
+        }, {
+          input: 'F',
+          output: 'FF'
+        }, {
+          input: null,
+          output: null
+        }
+      ],
+      iterations: 7,
+      angle: 30
+    },
+    {
+      id: 3,
+      name: 'Koch Curve',
+      axiom: 'FrrFrrF',
+      rules: [
+        {
+          input: 'F',
+          output: 'FlFrrFlF'
+        }, {
+          input: null,
+          output: null
+        }, {
+          input: null,
+          output: null
+        }
+      ],
+      iterations: 8,
+      angle: 60 + 25.8
+    }
+  ];
 });
---
+++ app/js/turtle.js
@@ -0,0 +1,252 @@
+var Turtle;
+var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
+
+Number.prototype.toDegrees = function() {
+  return (this * 180 / Math.PI).toFixed(1);
+};
+
+Turtle = (function() {
+  function Turtle(options) {
+    this.startingString = options.axiom;
+    this.rules = options.rules;
+    this.iterations = options.iterations;
+    this.d_radians = options.angle * (2 * Math.PI / 360);
+    this.distance = 5;
+    this.distanceMultiplier = options.distanceMultiplier || 1;
+    this.canvas = document.getElementById('main');
+    this.context = this.canvas.getContext('2d');
+    this.radians = 0;
+    this.string = options.axiom;
+    this.popList = [];
+    this.max = {
+      x: 0,
+      y: 0
+    };
+    this.min = {
+      x: 0,
+      y: 0
+    };
+    this.lastTrans = {
+      x: 0,
+      y: 0
+    };
+    this.scaler = 1;
+    this.pos = {
+      x: 0,
+      y: 0
+    };
+    this.points = [$.extend(true, {}, this.pos)];
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.generateString();
+    this.readString();
+    this.resizeCanvas();
+    this.draw();
+  }
+
+  Turtle.prototype.generateString = function() {
+    var letter, num, old, rule, ruleInputs, _results;
+    num = this.iterations;
+    _results = [];
+    while (num -= 1) {
+      ruleInputs = this.rules.map(function(x) {
+        return x.input;
+      });
+      old = this.string;
+      this.string = new String;
+      _results.push((function() {
+        var _i, _len, _results1;
+        _results1 = [];
+        for (_i = 0, _len = old.length; _i < _len; _i++) {
+          letter = old[_i];
+          if (__indexOf.call(ruleInputs, letter) >= 0) {
+            _results1.push((function() {
+              var _j, _len1, _ref, _results2;
+              _ref = this.rules;
+              _results2 = [];
+              for (_j = 0, _len1 = _ref.length; _j < _len1; _j++) {
+                rule = _ref[_j];
+                if (letter === rule.input) {
+                  _results2.push(this.string = this.string.concat(rule.output));
+                } else {
+                  _results2.push(void 0);
+                }
+              }
+              return _results2;
+            }).call(this));
+          } else {
+            _results1.push(this.string = this.string.concat(letter));
+          }
+        }
+        return _results1;
+      }).call(this));
+    }
+    return _results;
+  };
+
+  Turtle.prototype.goForward = function() {
+    this.pos.x += this.distance * Math.cos(this.radians);
+    this.pos.y += this.distance * Math.sin(this.radians);
+    this.points.push({
+      x: this.pos.x,
+      y: this.pos.y
+    });
+    if (this.pos.x > this.max.x) {
+      this.max.x = this.pos.x;
+    }
+    if (this.pos.y > this.max.y) {
+      this.max.y = this.pos.y;
+    }
+    if (this.pos.x < this.min.x) {
+      this.min.x = this.pos.x;
+    }
+    if (this.pos.y < this.min.y) {
+      return this.min.y = this.pos.y;
+    }
+  };
+
+  Turtle.prototype.turn = function(direction) {
+    if (direction === 'r') {
+      return this.radians = this.radians - this.d_radians;
+    } else if (direction === 'l') {
+      return this.radians = this.radians + this.d_radians;
+    }
+  };
+
+  Turtle.prototype.popIn = function() {
+    return this.popList.push({
+      radians: this.radians,
+      pos: {
+        x: this.pos.x,
+        y: this.pos.y
+      }
+    });
+  };
+
+  Turtle.prototype.popOut = function() {
+    var r;
+    r = this.popList.pop();
+    this.pos = r.pos;
+    this.radians = r.radians;
+    return this.points.push({
+      x: this.pos.x,
+      y: this.pos.y,
+      isNode: true
+    });
+  };
+
+  Turtle.prototype.readString = function() {
+    var letter, _i, _len, _ref, _results;
+    _ref = this.string;
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      letter = _ref[_i];
+      switch (letter) {
+        case 'F':
+          _results.push(this.goForward());
+          break;
+        case "[":
+          _results.push(this.popIn());
+          break;
+        case "]":
+          _results.push(this.popOut());
+          break;
+        case 'l':
+          _results.push(this.turn('l'));
+          break;
+        case 'r':
+          _results.push(this.turn('r'));
+          break;
+        default:
+          _results.push(void 0);
+      }
+    }
+    return _results;
+  };
+
+  Turtle.prototype.draw = function() {
+    var ctx, i, point, _i, _len, _ref;
+    ctx = this.context;
+    ctx.lineWidth = 0.5 / this.scaler;
+    ctx.lineJoin = 'round';
+    ctx.strokeStyle = 'white';
+    ctx.beginPath();
+    _ref = this.points;
+    for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
+      point = _ref[i];
+      if (point.isNode) {
+        ctx.moveTo(point.x, point.y);
+      } else {
+        ctx.lineTo(point.x, point.y);
+      }
+    }
+    ctx.stroke();
+    return this.resetTurtle();
+  };
+
+  Turtle.prototype.resetCanvas = function() {
+    this.context.translate(-this.lastTrans.x, -this.lastTrans.y);
+    return this.context.scale(1 / this.scaler, 1 / this.scaler);
+  };
+
+  Turtle.prototype.resetTurtle = function() {
+    this.resetCanvas();
+    this.string = this.startingString;
+    this.max = {
+      x: 0,
+      y: 0
+    };
+    this.min = {
+      x: 0,
+      y: 0
+    };
+    this.lastTrans = {
+      x: 0,
+      y: 0
+    };
+    this.scaler = 1;
+    this.pos = {
+      x: 0,
+      y: 0
+    };
+    return this.points = [$.extend(true, {}, this.pos)];
+  };
+
+  Turtle.prototype.resizeCanvas = function() {
+    var center, dx, dy, height, heightRatio, rulesString, text, width, widthRatio;
+    width = this.max.x - this.min.x;
+    height = this.max.y - this.min.y;
+    center = {
+      x: this.min.x + width / 2,
+      y: this.min.y + height / 2
+    };
+    widthRatio = this.canvas.width / width;
+    heightRatio = this.canvas.height / height;
+    this.scaler = Math.min.apply(null, [widthRatio, heightRatio, 15]) * 0.9;
+    rulesString = this.rules.map(function(z) {
+      return "" + z.input + "->" + z.output;
+    });
+    rulesString = rulesString.join(", ");
+    text = "iterations:  " + this.iterations + ", scale:     " + (this.scaler.toFixed(2)) + ", segments:   " + this.points.length;
+    $('#info span:first-child').text(text);
+    dx = -1 * (center.x - this.canvas.width / (2 * this.scaler));
+    dy = -1 * (center.y - this.canvas.height / (2 * this.scaler));
+    this.context.scale(this.scaler, this.scaler);
+    this.lastTrans.x = dx;
+    this.lastTrans.y = dy;
+    return this.context.translate(dx, dy);
+  };
+
+  Turtle.prototype.drawNextIteration = function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.iterations++;
+    customTimer(this.generateString, 'generate string', this);
+    customTimer(this.readString, 'find points', this);
+    this.resizeCanvas();
+    return customTimer(this.draw, 'draw', this);
+  };
+
+  return Turtle;
+
+})();
+
